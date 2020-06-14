a9f2612e956823bcd1b06f78f38b03a5f29ae388
redraw <agustinbv@gmail.com>
2020-06-14 15:47:31 -0300

    updated API

--- main.py
+++ main.py
@@ -6,17 +6,19 @@ import urequests
 from utils import load, save, log, unix_to_2000_epoch
 from config import LED_PIN, API, NORAD_ID, LAT, LON, PASS_FILE, ONLY_VISIBLE
 
+machine.Pin(2, machine.Pin.OUT, 0) # builtin led
+led = machine.Pin(LED_PIN, machine.Pin.OUT, 0)
+
 rtc = machine.RTC()
 rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
-
-machine.Pin(2, machine.Pin.OUT, 0) # builtin
-led = machine.Pin(LED_PIN, machine.Pin.OUT, 0)
 
 if machine.reset_cause() == machine.DEEPSLEEP_RESET:
     log("Wake up.")
 
+
 def query_next_pass():
-    response = urequests.get("%s/%s?lat=%s&lon=%s&limit=1" % (API, NORAD_ID, LAT, LON))
+    url = {"base": API, "lat": LAT, "lon": LON, "norad": NORAD_ID, "limit": 1}
+    response = urequests.get("{base}/{norad}?lat={lat}&lon={lon}&limit={limit}".format(**url))
     log("[%s] HTTP %s" % (API, response.status_code))
     if response.status_code >= 400:
         log(response.text)
@@ -24,11 +26,13 @@ def query_next_pass():
         machine.deepsleep()
     return response.json()[0]
 
+
 def schedule_next_pass():
     data = query_next_pass()
     save(PASS_FILE, data)
-    log("Next pass: %s" % data["start"]["datetime"])
+    log("Next pass: %s" % data["rise"]["utc_datetime"])
     return data
+
 
 def display_pass(seconds):
     log("Satellite passing! %s seconds" % seconds)
@@ -36,14 +40,15 @@ def display_pass(seconds):
     time.sleep(seconds)
     led.off()
 
+
 # Get a scheduled pass
 scheduled_pass = load(PASS_FILE) or schedule_next_pass()
-countdown = unix_to_2000_epoch(scheduled_pass["start"]["timestamp"]) - time.time()
+countdown = unix_to_2000_epoch(scheduled_pass["rise"]["utc_timestamp"]) - time.time()
 
 # If countdown less than 60s, display pass and reset
 if countdown < 60:
-    start = scheduled_pass["start"]["timestamp"]
-    end = scheduled_pass["end"]["timestamp"]
+    start = scheduled_pass["rise"]["utc_timestamp"]
+    end = scheduled_pass["end"]["utc_timestamp"]
     if not ONLY_VISIBLE or schedule_next_pass["visible"] and ONLY_VISIBLE:
         display_pass(end - start)
     os.remove(PASS_FILE)
